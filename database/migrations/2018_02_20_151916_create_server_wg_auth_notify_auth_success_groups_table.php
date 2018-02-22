3f7b7a2a6f6593c501e4b1d82b45cbafdf0e437b
Artem Ivanko <ivankoartem@gmail.com>
2018-02-22 20:43:31 +0200

    подготовка к релизу проекта

---
+++ database/migrations/2018_02_20_151916_create_server_wg_auth_notify_auth_success_groups_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServerWgAuthNotifyAuthSuccessGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('server_wg_auth_notify_auth_success_groups', function (Blueprint $table) {
+	        $table->increments('id');
+	        $table->unsignedInteger( 'server_id' )->default(0);
+	        $table->boolean( 'commander' )->default(0);
+	        $table->boolean( 'executive_officer' )->default(0);
+	        $table->boolean( 'personnel_officer' )->default(0);
+	        $table->boolean( 'combat_officer' )->default(0);
+	        $table->boolean( 'intelligence_officer' )->default(0);
+	        $table->boolean( 'quartermaster' )->default(0);
+	        $table->boolean( 'recruitment_officer' )->default(0);
+	        $table->boolean( 'junior_officer' )->default(0);
+	        $table->boolean( 'private' )->default(0);
+	        $table->boolean( 'recruit' )->default(0);
+	        $table->boolean( 'reservist' )->default(0);
+	        $table->timestamps();
+	        $table->foreign( 'server_id' )
+	              ->references( 'id' )->on( 'servers' )
+	              ->onDelete( 'cascade' )
+	              ->onUpdate( 'cascade' );
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('server_wg_auth_notify_auth_success_groups');
+    }
+}
