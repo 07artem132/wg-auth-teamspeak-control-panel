a97f9406c0763907548f20f28b7d7f172d074a18
Artem Ivanko <ivankoartem@gmail.com>
2018-03-02 13:26:14 +0200

    удаление лишней таблицы и добавление возможности указать примечание для сервера

---
+++ database/migrations/2018_02_23_143834_add_colum_name_servers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumNameServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::table('servers', function (Blueprint $table) {
+		    $table->string('name')->after('uid');
+	    });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::table('servers', function (Blueprint $table) {
+		    $table->dropColumn('name');
+	    });
+
+    }
+}
