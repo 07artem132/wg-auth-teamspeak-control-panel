c4fe3648da2a3cdbd2b8332e23a932d03dafcaa0
Artem Ivanko <ivankoartem@gmail.com>
2018-03-06 11:26:58 +0200

    багфиксы

--- app/Http/Controllers/TeamspeakWn8GroupController.php
+++ app/Http/Controllers/TeamspeakWn8GroupController.php
@@ -54,54 +54,56 @@ class TeamspeakWn8GroupController extends Controller {
 						}
 
 						switch ( true ) {
-							case $wn8 > 0 && $wn8 <= 949:
+							case $wn8 >= 0 && $wn8 <= 949:
 								$sgid = $tsClient->server->wn8->red_sg_id;
 								break;
-							case $wn8 > 950 && $wn8 <= 1549:
+							case $wn8 >= 950 && $wn8 <= 1549:
 								$sgid = $tsClient->server->wn8->yellow_sg_id;
 								break;
-							case $wn8 > 1550 && $wn8 <= 2349:
+							case $wn8 >= 1550 && $wn8 <= 2349:
 								$sgid = $tsClient->server->wn8->green_sg_id;
 								break;
-							case $wn8 > 2350 && $wn8 <= 3129:
+							case $wn8 >= 2350 && $wn8 <= 3129:
 								$sgid = $tsClient->server->wn8->turquoise_sg_id;
 								break;
-							case $wn8 > 3130 && $wn8 <= 9999 :
+							case $wn8 >= 3130 && $wn8 <= 9999 :
 								$sgid = $tsClient->server->wn8->purple_sg_id;
 								break;
 						}
-
-						if ( ! $TeamSpeak->ClientMemberOfServerGroupId( $tsClient->client_uid, $sgid ) ) {
-							foreach ( $module->options as $option ) {
-								if ( $option->option->name == 'message_type' ) {
-									if ( $option->value == 'poke' ) {
-										foreach ( $module->options as $option2 ) {
-											if ( $option2->option->name == 'message_success' ) {
-												foreach ( $module->options as $option3 ) {
-													if ( $option3->option->name == 'notify' && $option3->value == 'enable' ) {
-														$TeamSpeak->SendPokeClient( $tsClient->client_uid, $option2->value );
+						if ( ! empty( $sgid ) ) {
+							if ( ! $TeamSpeak->ClientMemberOfServerGroupId( $tsClient->client_uid, $sgid ) ) {
+								foreach ( $module->options as $option ) {
+									if ( $option->option->name == 'message_type' ) {
+										if ( $option->value == 'poke' ) {
+											foreach ( $module->options as $option2 ) {
+												if ( $option2->option->name == 'message_success' ) {
+													foreach ( $module->options as $option3 ) {
+														if ( $option3->option->name == 'notify' && $option3->value == 'enable' ) {
+															$TeamSpeak->SendPokeClient( $tsClient->client_uid, $option2->value );
+														}
 													}
+													$TeamSpeak->ClientAddServerGroup( $tsClient->client_uid, $sgid );
 												}
-												$TeamSpeak->ClientAddServerGroup( $tsClient->client_uid, $sgid );
 											}
-										}
 
-									} elseif ( $option->value == 'message' ) {
-										foreach ( $module->options as $option2 ) {
-											if ( $option2->option->name == 'message_success' ) {
-												foreach ( $module->options as $option3 ) {
-													if ( $option3->option->name == 'notify' && $option3->value == 'enable' ) {
-														$TeamSpeak->SendMessageClient( $tsClient->client_uid, $option2->value );
+										} elseif ( $option->value == 'message' ) {
+											foreach ( $module->options as $option2 ) {
+												if ( $option2->option->name == 'message_success' ) {
+													foreach ( $module->options as $option3 ) {
+														if ( $option3->option->name == 'notify' && $option3->value == 'enable' ) {
+															$TeamSpeak->SendMessageClient( $tsClient->client_uid, $option2->value );
+														}
 													}
+													$TeamSpeak->ClientAddServerGroup( $tsClient->client_uid, $sgid );
 												}
-												$TeamSpeak->ClientAddServerGroup( $tsClient->client_uid, $sgid );
 											}
 										}
 									}
 								}
 							}
 						}
-						$TeamSpeak->ReturnConnection()->execute('quit');
+						$TeamSpeak->ReturnConnection()->execute( 'quit' );
+
 					}
 				}
 			}
