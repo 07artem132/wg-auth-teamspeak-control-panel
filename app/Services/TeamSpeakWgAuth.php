f37e8d2bf9f9258ca4c1aba5ce38adb4cfeb67d3
Artem Ivanko <ivankoartem@gmail.com>
2018-03-07 09:53:26 +0200

    багфиксы

--- app/Services/TeamSpeakWgAuth.php
+++ app/Services/TeamSpeakWgAuth.php
@@ -58,7 +58,7 @@ class TeamSpeakWgAuth {
 	}
 
 	function GetVerifyDataByID( $VerifyID ) {
-		return Cache::get( "PendingVerify:$VerifyID" );
+		return Cache::pull( "PendingVerify:$VerifyID" );
 	}
 
 	function ClientUidIsRegister( $uid ) {
